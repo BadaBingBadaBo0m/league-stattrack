403fefa3b62912ae7cf93c3fd8a1ae6b5093453d
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2024-02-04 13:28:57 -0800

    match container now lines players in a grid depending on position and team side

--- react-app/src/MatchContainer/index.js
+++ react-app/src/MatchContainer/index.js
@@ -2,22 +2,30 @@ import React from 'react';
 import './MatchContainer.css';
 
 const MatchContainer = ({ match }) => {
+  let playerCount = 0;
+  const playerList = match.info.participants;
+
+  const printPlayerList = () => {
+    console.log(playerList)
+  }
 
   return (
     <div>
       <h1>match</h1>
 
-      <ul id='match-player-list-container'>
-        {
-          // Split into two different lists check which side the team was on to choose what side they go on
-          match.info.participants.map(player => (
-            <li>
-              <img id='match-champ-portrait' src={`/dragontail-14.2.1/14.2.1/img/champion/${player.championName}.png`} />
+      <ul id='match-player-list-container' >
+        {playerList.map(player => {
+          playerCount++;
+          return (
+            <li className={`lane_${playerCount} player-list-player`}>
+              <img className='match-champ-portrait' src={`/dragontail-14.2.1/14.2.1/img/champion/${player.championName}.png`} />
               <span>{player.riotIdGameName}</span>
             </li>
-          ))
-        }
+          )
+        })}
       </ul>
+
+      <button onClick={printPlayerList}>Print player list</button>
     </div>
   )
 };
