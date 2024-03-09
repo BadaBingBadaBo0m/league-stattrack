38d83112f96d972ec7b313c68d43a96365554afc
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2024-03-09 12:39:35 -0800

    cleaned up get_match_info route

--- app/api/riot_routes.py
+++ app/api/riot_routes.py
@@ -62,12 +62,6 @@ def get_match_info():
   
   matches = []
 
-  # match_info = get(f"https://americas.api.riotgames.com/lol/match/v5/matches/{match_list[0]}",
-  #   headers={
-  #   "X-Riot-Token": RIOT_API_KEY
-  # })
-  # matches.append(match_info.json())
-
   for match in match_list:
     match_info_response = get(f"https://americas.api.riotgames.com/lol/match/v5/matches/{match}",
     headers={
